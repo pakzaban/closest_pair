46f62173923f70e7bc4262e19db4ec7fc3adccc1
Peyman Pakzaban <pakzaban@live.com>
2021-01-02 01:54:33 -0600

    Final working version.

--- Point.py
+++ Point.py
@@ -7,12 +7,13 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
     def __str__(self):
         return f"({self.x}, {self.y})"
 
 def make_random_points(num):
-    """ returns a list of length num of random points with coordinates <1000"""
+    """
+    returns a list of length num of random points with coordinates <1000
+    """
     return [Point(random.randrange(1000), (random.randrange(1000))) for
             dummy in range(num)]
 
